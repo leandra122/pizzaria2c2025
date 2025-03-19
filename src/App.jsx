ff8a06b7207488373812508e37fa3a947a0a9b1f
leandra122 <rm94446@estudante.fieb.edu.br>
2025-03-19 08:27:52 -0300

    Componente

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ function App(){
 
   const [nome, setNome] = useState('Ronaldo')
 
-  return (
-     <div>
-      <h3>Pizzaria 2C</h3>
-      <input 
+  const Formulario = () => {
+
+    return (
+      <div>
+         <input 
       ClassName="nome"
       onChange={(e)=>{setNome(e.target.value)}}
       name="nome"
@@ -17,8 +18,18 @@ function App(){
       className="botao">
         CLIQUE AQUI
       </button>
+      </div>
+    )
+  }
+
+  return (
+     <div>
+      <h3>Pizzaria 2C</h3>
+     <Formulario />
+     <Formulario />
+     <Formulario />
      </div>
-  )
+  );
 }
 
-export default App
+export default App;
