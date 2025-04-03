6d5d97e52a9fa1063d8a6284b79f7e9efebcf525
leandra122 <rm94446@estudante.fieb.edu.br>
2025-04-03 08:25:49 -0300

    envio de parâmetros

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,38 @@ function App(){
 
   const [nome, setNome] = useState('Ronaldo')
 
-  const Formulario = () => {
+  const Formulario = (props) => {
 
     return (
       <div>
          <input 
+      placeholder={props.sombra}
       ClassName="nome"
       onChange={(e)=>{setNome(e.target.value)}}
       name="nome"
       type="text" />
       <button
-      onClick={()=>{alert(nome)}}
+      onClick={()=>{(props.mensagem) ? alert(props.mensagem)
+         : alert("TEXTO PADRÃO")}}
       className="botao">
-        CLIQUE AQUI
+      {(props.textoBotao != null) ? props.textoBotao: "CLIQUE AQUI"}
       </button>
       </div>
-    )
-  }
+    ) 
+};
 
   return (
      <div>
       <h3>Pizzaria 2C</h3>
-     <Formulario />
+     <Formulario  sombra="Digite seu nome..." textoBotao="ENVIAR NOME"/>
+     <Formulario  sombra="Digite seu e-mail..." textoBotao="E-MAIL"/>
+     <Formulario  sombra="(11) 90000-0000" textoBotao="CELULAR"/>
+     <Formulario  mensagem="Mensagem enviada pelo App"/>
      <Formulario />
      <Formulario />
      </div>
   );
-}
+
 
 export default App;
+0
