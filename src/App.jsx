235adf790806f0ec191c5809baacef5cac13c495
leandra122 <rm94446@estudante.fieb.edu.br>
2025-04-10 10:29:29 +0000

    vercel alterações

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,43 @@
-import { useState } from "react"
+import {useState} from "react"
 
 function App(){
 
-  const [nome, setNome] = useState('Ronaldo')
+  const [nome,setNome] = useState('Ronaldo')
 
-  const Formulario = (props) => {
-
+  const Formulario = () => {
     return (
       <div>
-         <input 
-      placeholder={props.sombra}
-      ClassName="nome"
+           <input
+      className="nome"
       onChange={(e)=>{setNome(e.target.value)}}
       name="nome"
       type="text" />
       <button
-      onClick={()=>{(props.mensagem) ? alert(props.mensagem)
-         : alert("TEXTO PADRÃO")}}
+      onClick={()=>{alert(nome)}}
       className="botao">
-      {(props.textoBotao != null) ? props.textoBotao: "CLIQUE AQUI"}
+      CLIQUE AQUI 
       </button>
       </div>
-    ) 
-};
+    )
+  }
 
-  return (
-     <div>
-      <h3>Pizzaria 2C</h3>
-     <Formulario  sombra="Digite seu nome..." textoBotao="ENVIAR NOME"/>
-     <Formulario  sombra="Digite seu e-mail..." textoBotao="E-MAIL"/>
-     <Formulario  sombra="(11) 90000-0000" textoBotao="CELULAR"/>
-     <Formulario  mensagem="Mensagem enviada pelo App"/>
-     <Formulario />
-     <Formulario />
-     </div>
-  );
+  return(
+
+    <div>
+      <h3>pizzaria 2c</h3>
+      <Formulario/>
+      <Formulario/>
+      <Formulario/>
 
 
-export default App;
-0
+    
+
+
+
+    </div>
+  )
+
+
+}
+
+export default App 
