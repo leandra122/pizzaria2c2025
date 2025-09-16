e1869ad8858fe6291dc961e5b7bac28c4eae2ca5
leandra122 <rm94446@estudante.fieb.edu.br>
2025-09-16 07:42:19 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,16 @@
-import { useState } from "react"
- 
-function App(){
- 
-  const [nome, setNome] = useState('Ronaldo')
- 
-  const Formulario = (props) => {
- 
-    return (
-      <div>
-         <input
-         placeholder={props.sombra}
-      ClassName="nome"
-      onChange={(e)=>{(props.mensagem != null)? alert (pros.mensagem) : alert("TEXTO PADRÃO")}}
-      name="nome"
-      type="text" />
-      <button
-      onClick={()=>{alert(nome)}}
-      className="botao">
-        {(props.textoBotao != null ? props.textoBotao:"CLIQUE AQUI")}
-      </button>
-      </div>
-    )
-  }
+import Produtos from "./pages/produtos";
+
+const App = () => {
+
  
   return (
-     <div>
-      <h3>Pizzaria 2C</h3>
-     <Formulario   sombra="digite seu nome..." textoBotao="ENVIAR NOME" />
-     <Formulario  sombra="Digite seu e-mail..." textoBotao="E-MAIL"/>
-     <Formulario  sombra="(11) 90000-0000" textoBotao="CELULAR"/>
-     <Formulario  mensagem="Mensagem enviada pelo App"/>
-     <Formulario />
-     <Formulario />
-     </div>
+    <div>
+      <h1>Projeto Pizzaria</h1>
+
+      <Produtos />
+
+    </div>
   );
 }
- 
+
 export default App;
- 
- 
---
+++ src/pages/produtos/index.jsx
@@ -0,0 +1,16 @@
+const Produtos = () => {
+
+
+    return (
+        <div>
+            <h3>Listagem de Produtos</h3>
+            <ul>
+                <li>Pizza de Calabresa</li>
+                <li>Pizza de Mussarela</li>
+                <li>Pizza Quatro Queijos</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Produtos
