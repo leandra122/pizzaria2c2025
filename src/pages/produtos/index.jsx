296b083b0406614d64c392e027339926875486a1
leandra122 <rm94446@estudante.fieb.edu.br>
2025-09-17 08:26:21 -0300

    lista dinâmica

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,13 +1,19 @@
 const Produtos = () => {
 
-
-    return (
+   //Objeto da lista de pizzas
+   const pizzas = [
+    'Pizza de Quatro Queijos',
+    'Pizza de Frango com Catupiry',
+    'Pizza de Marguerita', 'Pizza de pepperoni', 'Pizza de estrogonofe'
+   ]
+   // Iteração da lista de pizzas - pizza a pizza (um a um)
+   const listaPizzas = pizzas.map(pizza=><li>{pizza}</li>)
+   
+   return (
         <div>
             <h3>Listagem de Produtos</h3>
             <ul>
-                <li>Pizza de Calabresa</li>
-                <li>Pizza de Mussarela</li>
-                <li>Pizza Quatro Queijos</li>
+              {listaPizzas}
             </ul>
         </div>
     )
